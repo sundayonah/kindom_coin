86b382a73d5037d37bcea04bb3b6c0ae00086fb3
sundayonah <sundayonah94@gmail.com>
2023-04-24 21:04:14 +0100

    update to mainnet

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ import { TransactionProvider } from "../Components/TransactionContext";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChainId = ChainId.BinanceSmartChainTestnet;
+const activeChainId = ChainId.BinanceSmartChainMainnet;
 
 function MyApp({ Component, pageProps }) {
   return (
